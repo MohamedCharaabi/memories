146f2257c6547e9c840fc2ba8dddad7aa07a52a7
MohamedCharaabi <medch373@gmail.com>
2021-01-25 10:50:04 +0100

    finishing first setup

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -14,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+// const CONNECTION_URL = 'mongodb+srv://med:2v4pekhfYIqq1XaA@cluster0.ve80h.mongodb.net/memo?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
